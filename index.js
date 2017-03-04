090330551701e75e242dcf5fccacb009ca833d92
Jake Helme <jake.helme@gmail.com>
2017-03-04 11:58:10 +0200

    index.js will simply initiate the first stage of a rocket on the launch pad

---
+++ index.js
@@ -0,0 +1,66 @@
+'use strict';
+
+let util = require('util');
+let Client = require('krpc-node');
+let host = process.argv[2];
+let client = Client({host: host});
+let game = {};
+
+client.rpc.on('open', function (event) {
+    client.rpc.on('message', getActiveVesselComplete);
+    client.rpc.send(client.services.spaceCenter.getActiveVessel());
+});
+
+client.rpc.on('error', function (err) {
+    console.log(util.format('Error : %j', err));
+    process.exit(1);
+});
+
+client.rpc.on('close', function (event) {
+    console.log(util.format('Connection Closed : %j', event));
+    process.exit(1);
+});
+
+function getActiveVesselComplete(response) {
+    game.vessel = {
+        id: getFirstResult(response)
+    };
+    replaceMessageHandler(getActiveVesselControlComplete);
+    client.rpc.send(client.services.spaceCenter.vesselGetControl(game.vessel.id));
+}
+
+function getActiveVesselControlComplete(response) {
+    game.vessel.control = {
+        id: getFirstResult(response)
+    };
+    replaceMessageHandler(getThrottleValueComplete);
+    client.rpc.send(client.services.spaceCenter.controlGetThrottle(game.vessel.control.id));
+}
+
+function getThrottleValueComplete(response) {
+    game.vessel.control.throttle = getFirstResult(response);
+    console.log(util.format("Updating throttle value from %s to 1", game.vessel.control.throttle));
+    replaceMessageHandler(setThrottleToFullComplete);
+    var call = client.services.spaceCenter.controlSetThrottle(game.vessel.control.id, 1);
+    client.rpc.send(call);
+}
+
+function setThrottleToFullComplete(response) {
+    replaceMessageHandler(launched);
+    client.rpc.send(client.services.spaceCenter.controlActivateNextStage(game.vessel.control.id));
+}
+
+function launched(response) {
+    console.log("launched!!");
+    process.exit(0);
+}
+
+function getFirstResult(response) {
+    var result = response.results[0];
+    return result.value;
+}
+
+function replaceMessageHandler(fn) {
+    client.rpc.emitter.removeAllListeners('message');
+    client.rpc.on('message', fn);
+}
