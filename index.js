f45317f09c8a2a8ed5ffa995663987db6544675c
Jake Helme <jake.helme@gmail.com>
2017-03-05 10:39:52 +0200

    Testing board

--- index.js
+++ index.js
@@ -24,12 +24,8 @@ let options = {
     wsOptions: null
   }
 };
-let client = Client(options);
-let game = {};
 
-client.rpc.on('open', function (event) {
-  client.rpc.on('message', getActiveVesselComplete);
-  board.on("ready", function () {
+board.on("ready", function () {
     var led = new five.Led("P1-35");
     var toggle = new five.Switch("P1-7");
     let isOpen;
@@ -60,59 +56,67 @@ client.rpc.on('open', function (event) {
     }
 
   });
-  client.rpc.send(client.services.spaceCenter.getActiveVessel());
-});
 
-client.rpc.on('error', function (err) {
-  console.log(util.format('Error : %j', err));
-  process.exit(1);
-});
 
-client.rpc.on('close', function (event) {
-  console.log(util.format('Connection Closed : %j', event));
-  process.exit(1);
-});
+// let client = Client(options);
+// let game = {};
 
-function getActiveVesselComplete(response) {
-  game.vessel = {
-    id: getFirstResult(response)
-  };
-  replaceMessageHandler(getActiveVesselControlComplete);
-  client.rpc.send(client.services.spaceCenter.vesselGetControl(game.vessel.id));
-}
+// client.rpc.on('open', function (event) {
+//   client.rpc.on('message', getActiveVesselComplete);
+  
+//   client.rpc.send(client.services.spaceCenter.getActiveVessel());
+// });
 
-function getActiveVesselControlComplete(response) {
-  game.vessel.control = {
-    id: getFirstResult(response)
-  };
-  replaceMessageHandler(getThrottleValueComplete);
-  client.rpc.send(client.services.spaceCenter.controlGetThrottle(game.vessel.control.id));
-}
+// client.rpc.on('error', function (err) {
+//   console.log(util.format('Error : %j', err));
+//   process.exit(1);
+// });
 
-function getThrottleValueComplete(response) {
-  game.vessel.control.throttle = getFirstResult(response);
-  console.log(util.format("Updating throttle value from %s to 1", game.vessel.control.throttle));
-  replaceMessageHandler(setThrottleToFullComplete);
-  var call = client.services.spaceCenter.controlSetThrottle(game.vessel.control.id, 1);
-  client.rpc.send(call);
-}
+// client.rpc.on('close', function (event) {
+//   console.log(util.format('Connection Closed : %j', event));
+//   process.exit(1);
+// });
 
-function setThrottleToFullComplete(response) {
-  replaceMessageHandler(launched);
-  client.rpc.send(client.services.spaceCenter.controlActivateNextStage(game.vessel.control.id));
-}
+// function getActiveVesselComplete(response) {
+//   game.vessel = {
+//     id: getFirstResult(response)
+//   };
+//   replaceMessageHandler(getActiveVesselControlComplete);
+//   client.rpc.send(client.services.spaceCenter.vesselGetControl(game.vessel.id));
+// }
 
-function launched(response) {
-  console.log("launched!!");
-  process.exit(0);
-}
+// function getActiveVesselControlComplete(response) {
+//   game.vessel.control = {
+//     id: getFirstResult(response)
+//   };
+//   replaceMessageHandler(getThrottleValueComplete);
+//   client.rpc.send(client.services.spaceCenter.controlGetThrottle(game.vessel.control.id));
+// }
 
-function getFirstResult(response) {
-  var result = response.results[0];
-  return result.value;
-}
+// function getThrottleValueComplete(response) {
+//   game.vessel.control.throttle = getFirstResult(response);
+//   console.log(util.format("Updating throttle value from %s to 1", game.vessel.control.throttle));
+//   replaceMessageHandler(setThrottleToFullComplete);
+//   var call = client.services.spaceCenter.controlSetThrottle(game.vessel.control.id, 1);
+//   client.rpc.send(call);
+// }
 
-function replaceMessageHandler(fn) {
-  client.rpc.emitter.removeAllListeners('message');
-  client.rpc.on('message', fn);
-}
+// function setThrottleToFullComplete(response) {
+//   replaceMessageHandler(launched);
+//   client.rpc.send(client.services.spaceCenter.controlActivateNextStage(game.vessel.control.id));
+// }
+
+// function launched(response) {
+//   console.log("launched!!");
+//   process.exit(0);
+// }
+
+// function getFirstResult(response) {
+//   var result = response.results[0];
+//   return result.value;
+// }
+
+// function replaceMessageHandler(fn) {
+//   client.rpc.emitter.removeAllListeners('message');
+//   client.rpc.on('message', fn);
+// }
