0e538f4c4c20284779a63b11f0a0835dd496ef27
Jake Helme <jake.helme@gmail.com>
2017-03-05 11:50:23 +0200

    Going to try a toggle switch launch now

--- index.js
+++ index.js
@@ -26,97 +26,95 @@ let options = {
 };
 
 board.on("ready", function () {
-    var led = new five.Led("P1-35");
-    var toggle = new five.Switch("P1-7");
-    let isOpen;
+  var led = new five.Led("P1-35");
+  var toggle = new five.Switch("P1-7");
+  let isOpen;
 
-    board.repl.inject({
-      toggle: toggle,
-      led: led
-    });
+  let client = Client(options);
+  let game = {};
 
-    toggle.on("close", handleClose);
-
-    toggle.on("open", handleOpen);
-
-    function handleClose() {
-      if (isOpen) {
-        isOpen = false;
-        led.off();
-        console.log("LED off");
-      }
-    }
-
-    function handleOpen() {
-      if (!isOpen) {
-        isOpen = true;
-        led.on();
-        console.log("LED on");
-      }
-    }
-
+  client.rpc.on('error', function (err) {
+    console.log(util.format('Error : %j', err));
+    process.exit(1);
   });
 
+  client.rpc.on('close', function (event) {
+    console.log(util.format('Connection Closed : %j', event));
+    process.exit(1);
+  });
 
-// let client = Client(options);
-// let game = {};
+  board.repl.inject({
+    toggle: toggle,
+    led: led
+  });
 
-// client.rpc.on('open', function (event) {
-//   client.rpc.on('message', getActiveVesselComplete);
-  
-//   client.rpc.send(client.services.spaceCenter.getActiveVessel());
-// });
+  toggle.on("close", handleClose);
 
-// client.rpc.on('error', function (err) {
-//   console.log(util.format('Error : %j', err));
-//   process.exit(1);
-// });
+  toggle.on("open", handleOpen);
 
-// client.rpc.on('close', function (event) {
-//   console.log(util.format('Connection Closed : %j', event));
-//   process.exit(1);
-// });
+  function handleClose() {
+    if (isOpen) {
+      isOpen = false;
+      led.off();
 
-// function getActiveVesselComplete(response) {
-//   game.vessel = {
-//     id: getFirstResult(response)
-//   };
-//   replaceMessageHandler(getActiveVesselControlComplete);
-//   client.rpc.send(client.services.spaceCenter.vesselGetControl(game.vessel.id));
-// }
+    }
+  }
 
-// function getActiveVesselControlComplete(response) {
-//   game.vessel.control = {
-//     id: getFirstResult(response)
-//   };
-//   replaceMessageHandler(getThrottleValueComplete);
-//   client.rpc.send(client.services.spaceCenter.controlGetThrottle(game.vessel.control.id));
-// }
+  function handleOpen() {
+    if (!isOpen) {
+      isOpen = true;
+      led.on();
+      client.rpc.on('message', getActiveVesselComplete);
+      client.rpc.send(client.services.spaceCenter.getActiveVessel());
+    }
+  }
 
-// function getThrottleValueComplete(response) {
-//   game.vessel.control.throttle = getFirstResult(response);
-//   console.log(util.format("Updating throttle value from %s to 1", game.vessel.control.throttle));
-//   replaceMessageHandler(setThrottleToFullComplete);
-//   var call = client.services.spaceCenter.controlSetThrottle(game.vessel.control.id, 1);
-//   client.rpc.send(call);
-// }
+  function getActiveVesselComplete(response) {
+    game.vessel = {
+      id: getFirstResult(response)
+    };
+    replaceMessageHandler(getActiveVesselControlComplete);
+    client.rpc.send(client.services.spaceCenter.vesselGetControl(game.vessel.id));
+  }
 
-// function setThrottleToFullComplete(response) {
-//   replaceMessageHandler(launched);
-//   client.rpc.send(client.services.spaceCenter.controlActivateNextStage(game.vessel.control.id));
-// }
+  function getActiveVesselControlComplete(response) {
+    game.vessel.control = {
+      id: getFirstResult(response)
+    };
+    replaceMessageHandler(getThrottleValueComplete);
+    client.rpc.send(client.services.spaceCenter.controlGetThrottle(game.vessel.control.id));
+  }
 
-// function launched(response) {
-//   console.log("launched!!");
-//   process.exit(0);
-// }
+  function getThrottleValueComplete(response) {
+    game.vessel.control.throttle = getFirstResult(response);
+    console.log(util.format("Updating throttle value from %s to 1", game.vessel.control.throttle));
+    replaceMessageHandler(setThrottleToFullComplete);
+    var call = client.services.spaceCenter.controlSetThrottle(game.vessel.control.id, 1);
+    client.rpc.send(call);
+  }
 
-// function getFirstResult(response) {
-//   var result = response.results[0];
-//   return result.value;
-// }
+  function setThrottleToFullComplete(response) {
+    replaceMessageHandler(launched);
+    client.rpc.send(client.services.spaceCenter.controlActivateNextStage(game.vessel.control.id));
+  }
 
-// function replaceMessageHandler(fn) {
-//   client.rpc.emitter.removeAllListeners('message');
-//   client.rpc.on('message', fn);
-// }
+  function launched(response) {
+    console.log("launched!!");
+    process.exit(0);
+  }
+
+  function getFirstResult(response) {
+    var result = response.results[0];
+    return result.value;
+  }
+
+  function replaceMessageHandler(fn) {
+    client.rpc.emitter.removeAllListeners('message');
+    client.rpc.on('message', fn);
+  }
+
+});
+
+
+
+
